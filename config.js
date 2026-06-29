dfc446e938ad95a1c78edd06e188fa59e0c1a49b
IsaccLopesADM <lopescellpfo@gmail.com>
2026-06-29 13:19:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // Nunca coloque service_role aqui.
 // ========================================================
 
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_CHAVE_PUBLICA_AQUI";
+const SUPABASE_URL = "https://oejwpufhbdjlaxesorwi.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_wIXDeWADQf0K42FgybzZDA_HIdUFVgk"
 
 const APP_DEVICE_NAME = "Tablet Refeitório 01";
 const APP_DEVICE_TYPE = "tablet";
